d55b9242d0cb61ec12d322f9c8761c9fc7c2e0dc
RRaa <35782666+RRaa@users.noreply.github.com>
2019-05-09 10:02:57 +0300

    Ranking players

---
+++ src/PlayerSorter.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PlayerSorter {
+    public static void main(String[] args) {
+        List<Player> footballTeam = new ArrayList<>();
+
+        Player player1 = new Player(59, "John", 20);
+        Player player2 = new Player(24, "Bob", 24);
+        Player player3 = new Player(35, "Michael", 12);
+
+        footballTeam.add(player1);
+        footballTeam.add(player2);
+        footballTeam.add(player3);
+
+        System.out.println("Before sorting: ");
+        for (Player pl : footballTeam) {
+            System.out.println("Age: "+pl.getAge()+", Name: "+pl.getName()+", Ranking: "+pl.getRanking());
+        }
+    }
+}
